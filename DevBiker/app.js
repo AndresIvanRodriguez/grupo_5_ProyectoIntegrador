68541638ead057fecbd131f00586865520c9960c
Andres Ivan Rodriguez Morales <airodriguezmo@unal.edu.co>
2021-12-21 16:49:36 -0500

    Andres Rodriguez sube actualizaciones

--- DevBiker/app.js
+++ DevBiker/app.js
@@ -10,3 +10,5 @@ app.listen(3001, () => console.log("Conexión exitosa"));
 
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")));
+app.get("/product", (req, res) => res.sendFile(path.resolve(__dirname, "./views/producto.html")));
+
