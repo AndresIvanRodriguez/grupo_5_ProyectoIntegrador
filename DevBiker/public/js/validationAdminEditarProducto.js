fe537331b136c27fb36fd7cc05181cb2fe73b4fc
juandiegoq56 <juandiegoq56@gmail.com>
2022-04-13 05:27:57 -0500

    funcionando todo menos el edit de productos

--- DevBiker/public/js/validationAdminEditarProducto.js
+++ DevBiker/public/js/validationAdminEditarProducto.js
@@ -1,83 +1,70 @@
-
-
 window.addEventListener("load", function(){
     // console.log('hola desde register' );
- 
      let formulario= document.getElementById("edit");
      formulario.addEventListener("submit",function(e){
- 
          let errores=[];
          //let aviso="";
- 
          let campoNombre=document.getElementById("nombre");
-         if(campoNombre.value == " " || campoNombre.value=="" ){
-             errores.push('fill in the name field');
+         if((campoNombre.value.length <=2 && campoNombre.value.length>=1  )){
+             errores.push('El nombre debe tener mas de dos caracteres');
+         }
+         else if(campoNombre.value=="" ){
+             errores.push('El nombre no puedeser nulo');
          }
          let campoValor =document.getElementById("valor");
          if(campoValor.value == " " ||campoValor.value =="" ) {
-             errores.push('fill in the cost field');
+             errores.push('El valor no puedeser nulo');
          }
          let campoColor=document.getElementById("color");
          if(campoColor.value == " "  || campoColor.value == ""){
-             errores.push('fill the field with color');
+             errores.push('El color no puedeser nulo');
          }
          let campoDescuento=document.getElementById("descuento");
          if(campoDescuento.value == " " || campoDescuento.value==""){
-             errores.push('fill in the discount field')
+             errores.push('El descuento no puedeser nulo')
          }
          let campoStock=document.getElementById("stock");
          if(campoStock.value == " " || campoStock.value==""){
-             errores.push('fill in the quantity field')
+             errores.push('El stock no puedeser nulo')
          }
          let campoCategoria=document.getElementById("categoria");
          if(campoCategoria.value == "Elige una categoria"  || campoCategoria.value == "------------------" ){
-             errores.push('fill in the category field')
+             errores.push('La categoria no puedeser nula')
          }
          let campoGenero=document.getElementById("genero");
          if(campoGenero.value == "Elige un genero" || campoGenero.value =="------------------"){
-             errores.push('fill in the gender field')
+             errores.push('El genero no puedeser nulo')
          }
          let campoImagen=document.getElementById("imagen");
          if(campoImagen.value== "" || campoImagen.value==" "){
-            errores.push('fill the image field')
+            errores.push('La imagen no puede estar vacía')
          }
          let campoDescripcion=document.getElementById("descripcion");
          if(campoDescripcion.value=="" || campoDescripcion.value==" "){
-             errores.push('fill en the description field')
+             errores.push('La descripción no puede estar vacía')
          }
          let campoMarco=document.getElementById("marco");
          if(campoMarco.value=="" || campoMarco.value==" "){
-             errores.push('fill in the frame field')
+             errores.push('El campo marco no puede estar vacío')
          }
          let campoCamibios=document.getElementById("camibios");
          if(campoCamibios.value=="" || campoCamibios.value==" "){
-             errores.push('fill in the changen field')
+             errores.push('El campo no puede estar vacía')
          }
          let campoFrenos=document.getElementById("frenos");
          if(campoFrenos.value==""  || campoFrenos.value==" "){
-             errores.push('fill in the brake field')
+             errores.push('El campo Frenos no puede estar vacío')
          }
          let campoSuspension=document.getElementById("suspension");
          if(campoSuspension.value==""  || campoSuspension.value==" "){
-             errores.push('fill in the suspension field')
+             errores.push('Indique el tipo de suspensión')
          }
-         
-        
- 
- 
          if (errores.length > 0){
              e.preventDefault();
- 
              let nombr= document.getElementById("nombr");
- 
              for (let i=0 ; i<errores.length ; i++){
- 
                  nombr.innerHTML+= "<p>"+errores[i]+"</p>"
              }
          }
- 
      })
- 
- 
  });
- 
--- DevBiker/public/js/validationLogin.js
+++ DevBiker/public/js/validationLogin.js
@@ -1,38 +1,78 @@
-
 window.addEventListener("load", function(){
-
     let formulario= document.getElementById("login");
     formulario.addEventListener("submit",function(e){
-
         let errores=[];
         //let aviso="";
-
-
         let campoEmail=document.getElementById("email");
-        if(campoEmail.value == " "  || campoEmail.value == ""){
-            errores.push('fill in the email field');
-            alert('hola')
-
+        let array= campoEmail.value.indexOf("@",0);
+        if(array ==-1 || campoEmail.value=="" ){
+            errores.push('email no valido');
         }
         let campopassword=document.getElementById("password");
-        if(campopassword.value == " " || campopassword.value==""){
+        var letras_mayusculas="ABCDEFGHYJKLMNÑOPQRSTUVWXYZ";
+        function tiene_mayusculas(texto){
+            for(i=0; i<texto.length; i++){
+               if (letras_mayusculas.indexOf(texto.charAt(i),0)!=-1){
+                  return true;
+               }
+            }
+            return false;
+         }
+         var letras="abcdefghyjklmnñopqrstuvwxyz";
+         function tiene_minusculas(texto){
+            for(i=0; i<texto.length; i++){
+               if (letras.indexOf(texto.charAt(i),0)!=-1){
+                  return true;
+               }
+            }
+            return false;
+         }
+         var numeros="0123456789";
+        function tiene_numeros(texto){
+         for(i=0; i<texto.length; i++){
+         if (numeros.indexOf(texto.charAt(i),0)!=-1){
+         return true;
+      }
+   }
+   return false;
+}
+var caracteres ="!|#$%&/()=?'¡¿+*~^,;.-_{}[]<>-"
+function tiene_caracteres(texto){
+    for(i=0; i<texto.length; i++){
+    if (caracteres.indexOf(texto.charAt(i),0)!=-1){
+    return true;
+ }
+}
+return false;
+}
+if((campopassword.value.length <=8 && campopassword.value.length>=1  )){
+    errores.push(' La contraseña  debe tener mas de 8 caracteres');
+}
+else if(campopassword.value=="" ){
+    errores.push('fill in the password field');
+}
+else if (tiene_mayusculas(campopassword.value)==false){
+    errores.push('contraseña debe tener almenos una mayuscula');
+}
+else if (tiene_minusculas(campopassword.value)==false){
+    errores.push('contraseña debe tener almenos una minuscula');
+}
+else if (tiene_numeros(campopassword.value)==false){
+    errores.push('contraseña debe tener almenos un numero');
+}
+else if (tiene_caracteres(campopassword.value)==false){
+    errores.push('contraseña debe tener almenos un caracter');
+}
+        /*if(campopassword.value == " " || campopassword.value==""){
             errores.push('fill in the key field')
         }
-
-
-
+*/
         if (errores.length > 0){
             e.preventDefault();
-
             let nombr= document.getElementById("nombr");
-
             for (let i=0 ; i<errores.length ; i++){
-
                 nombr.innerHTML+= "<p>"+errores[i]+"</p>"
             }
         }
-
     })
-
-
 });
